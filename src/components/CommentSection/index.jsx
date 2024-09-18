cf17b4c5f7aa9aba94c6078c32e2643561e121e3
baocd2003 <BaoCao494@gmail.com>
2024-09-18 19:09:07 +0700

    project_detail_package

---
+++ src/components/CommentSection/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CommentBar from './CommentBar'
+const CommentSection = () => {
+  return (
+    <>
+        <CommentBar />
+    </>
+  )
+}
+
+export default CommentSection

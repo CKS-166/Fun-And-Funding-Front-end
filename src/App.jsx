77daf3c66fa9032932213b0b25734e4a41e11b1c
baocd2003 <BaoCao494@gmail.com>
2024-09-24 15:21:32 +0700

    up_test_create

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import FunFundingAppBar from './components/AppBar'
 import "@fontsource/poppins"; // Defaults to weight 400
 import "@fontsource/poppins/300.css"; // Specify weight
 import "@fontsource/poppins/300-italic.css"; // Specify weight and style
+import { Navigate, Route, Routes } from "react-router-dom";
+import TestCR from './pages/TestCR';
+import TestDetail from './pages/TestDetail';
 function App() {
 
   return (
     <>
-    <FunFundingAppBar/>
-    <ProjectDetail/>
+    <Routes>
+      <Route path='/test' element={<TestCR/>}/>
+      <Route path='/' element={<TestDetail/>} />
+    </Routes>
+
     </>
   )
 }

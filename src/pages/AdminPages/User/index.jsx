5edd8981299384d5413f31d40da5555b8e245a7d
diemyolo <quydiem2015@gmail.com>
2024-11-17 00:41:52 +0700

    Admin-ManageUser-List+BlockUser

--- src/pages/AdminPages/User/index.jsx
+++ src/pages/AdminPages/User/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import userApiInstance from "../../../utils/ApiInstance/userApiInstance";
+import UserDetailModal from './UserDetailModal';
 
 const userStatus = [
     "Inactive",
@@ -10,6 +11,8 @@ function Users() {
     const [userList, setUserList] = useState([]);
     const [pageIndex, setPageIndex] = useState(1);
     const [totalPage, setTotalPage] = useState(1);
+    const [openModal, setOpenModal] = useState(false);
+    const [selectedUserId, setSelectedUserId] = useState("");
 
     useEffect(() => {
         fetchUserList();
@@ -37,58 +40,97 @@ function Users() {
         return new Intl.NumberFormat('de-DE').format(price);
     };
 
+    const handleOpenModal = (userId) => {
+        setSelectedUserId(userId);
+        setOpenModal(true);
+    };
+
     return (
         <>
-            <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
+            <div className="mx-auto max-w-screen-xl px-4 lg:px-12 my-8">
                 <div className="bg-white relative shadow-md sm:rounded-lg overflow-hidden">
                     <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                         <div className="w-full md:w-1/2">
-                            <span className="text-sm font-normal text-gray-500">
-                                Showing{" "}
+                            <span className="text-base font-normal text-gray-500">
+                                Showing total of{" "}
                                 <span className="font-semibold text-gray-900">
-                                    {userList.totalItems} {" "}
+                                    {userList.totalItems ?? '0'}{" "}
                                 </span>
-                                Users
+                                Accounts
                             </span>
                         </div>
-                        <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                            <div className="flex items-center space-x-3 w-full md:w-auto">
-                                <button
-                                    id="filterDropdownButton"
-                                    data-dropdown-toggle="filterDropdown"
-                                    className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
-                                    type="button"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        aria-hidden="true"
-                                        className="h-4 w-4 mr-2 text-gray-400"
-                                        viewbox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-                                            clip-rule="evenodd"
-                                        />
-                                    </svg>
-                                    Filter
-                                    <svg
-                                        className="-mr-1 ml-1.5 w-5 h-5"
-                                        fill="currentColor"
-                                        viewbox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        aria-hidden="true"
-                                    >
-                                        <path
-                                            clip-rule="evenodd"
-                                            fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
+                    </div>
+                    <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                        <div className="w-full md:w-1/2">
+                            <form className="flex items-center">
+                                <label for="simple-search" className="sr-only">Search</label>
+                                <div className="relative w-full">
+                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full pl-10 p-2" placeholder="Search" required="" />
+                                </div>
+                            </form>
                         </div>
+                        <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                            <button
+                                type="button"
+                                className="flex items-center justify-center text-white bg-[var(--primary-green)] hover:bg-[#159653] focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2"
+                            >
+                                <svg
+                                    className="h-4 w-4 mr-2"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        fillRule="evenodd"
+                                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    />
+                                </svg>
+                                Add product
+                            </button>
+
+                            <button
+                                id="filterDropdownButton"
+                                data-dropdown-toggle="filterDropdown"
+                                className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                                type="button"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                    className="h-4 w-4 mr-2 text-gray-400"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        fillRule="evenodd"
+                                        d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
+                                    />
+                                </svg>
+                                Filter
+                                <svg
+                                    className="-mr-1 ml-1.5 w-5 h-5"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        fillRule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+
                     </div>
                     <div className="overflow-x-auto">
                         <table className="w-full text-sm text-left text-gray-500">
@@ -98,7 +140,7 @@ function Users() {
                                         Email
                                     </th>
                                     <th scope="col" className="px-4 py-3">
-                                        Usermame
+                                        Username
                                     </th>
                                     <th scope="col" className="px-4 py-3">
                                         Fullname
@@ -118,49 +160,54 @@ function Users() {
                                 </tr>
                             </thead>
                             <tbody>
-                                {userList.items != null && userList.items.length > 0 && userList.items.map((user, index) => (
-                                    <tr key={index} className="border-b">
-                                        <th
-                                            scope="row"
-                                            className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap"
-                                        >
-                                            {user.email}
-                                        </th>
-                                        <td className="px-4 py-3">
-                                            {user.userName}
-                                        </td>
-                                        <td className="px-4 py-3">
-                                            {user.fullName}
-                                        </td>
-                                        <td className="px-4 py-3">
-                                            {new Date(user.createdDate).toLocaleString()}
-                                        </td>
-                                        <td className="px-4 py-3">
-                                            {formatPrice(user.wallet?.balance)} VND
-                                        </td>
-                                        <td className="px-4 py-3">
-                                            <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
-                                                {userStatus[user.userStatus]}
-                                            </span>
-                                        </td>
-                                        <td className="px-4 py-3 flex items-center justify-end">
-                                            <button
-                                                className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none"
-                                                type="button"
+                                {userList.items &&
+                                    userList.items.length > 0 &&
+                                    userList.items.map((user, index) => (
+                                        <tr key={index} className="border-b">
+                                            <th
+                                                scope="row"
+                                                className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap"
                                             >
-                                                <svg
-                                                    className="w-5 h-5"
-                                                    aria-hidden="true"
-                                                    fill="currentColor"
-                                                    viewBox="0 0 20 20"
-                                                    xmlns="http://www.w3.org/2000/svg"
+                                                {user.email}
+                                            </th>
+                                            <td className="px-4 py-3">{user.userName}</td>
+                                            <td className="px-4 py-3">{user.fullName}</td>
+                                            <td className="px-4 py-3">
+                                                {new Date(user.createdDate).toLocaleString()}
+                                            </td>
+                                            <td className="px-4 py-3">
+                                                {formatPrice(user.wallet?.balance)} <span className="text-[0.75rem]">VND</span>
+                                            </td>
+                                            <td className="px-4 py-3">
+                                                <span
+                                                    className={`text-xs font-medium me-2 px-2.5 py-0.5 rounded ${user.userStatus === 1
+                                                        ? "bg-[var(--primary-green)] text-[var(--white)]"
+                                                        : "bg-[var(--red)] text-[var(--white)]"
+                                                        }`}
                                                 >
-                                                    <path d="M17.414 2.586a2 2 0 00-2.828 0l-9 9A2 2 0 005 14v3a1 1 0 001 1h3a2 2 0 001.414-.586l9-9a2 2 0 000-2.828l-2-2zM7 14v-1.586L14.586 5l1.586 1.586L8.586 15H7z" />
-                                                </svg>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
+                                                    {userStatus[user.userStatus]}
+                                                </span>
+                                            </td>
+
+                                            <td className="px-4 py-3 flex items-center justify-end">
+                                                <button
+                                                    className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none"
+                                                    type="button"
+                                                    onClick={() => handleOpenModal(user.id)}
+                                                >
+                                                    <svg
+                                                        className="w-5 h-5"
+                                                        aria-hidden="true"
+                                                        fill="currentColor"
+                                                        viewBox="0 0 20 20"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                    >
+                                                        <path d="M17.414 2.586a2 2 0 00-2.828 0l-9 9A2 2 0 005 14v3a1 1 0 001 1h3a2 2 0 001.414-.586l9-9a2 2 0 000-2.828l-2-2zM7 14v-1.586L14.586 5l1.586 1.586L8.586 15H7z" />
+                                                    </svg>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
                             </tbody>
                         </table>
                     </div>
@@ -180,13 +227,13 @@ function Users() {
                                         className="w-5 h-5"
                                         aria-hidden="true"
                                         fill="currentColor"
-                                        viewbox="0 0 20 20"
+                                        viewBox="0 0 20 20"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
-                                            fill-rule="evenodd"
+                                            fillRule="evenodd"
                                             d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                            clip-rule="evenodd"
+                                            clipRule="evenodd"
                                         />
                                     </svg>
                                 </a>
@@ -257,6 +304,12 @@ function Users() {
                     </nav>
                 </div>
             </div>
+            <UserDetailModal
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+                selectedUserId={selectedUserId}
+                fetchUserList={fetchUserList}
+            />
         </>
     )
 }

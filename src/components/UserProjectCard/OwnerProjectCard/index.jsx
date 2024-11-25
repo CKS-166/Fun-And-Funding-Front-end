fb1d43ec60f5290f8c87cabce00b28a3c3485789
DaoHuongThao <daothao148@gmail.com>
2024-11-25 11:40:39 +0700

    add coupons

--- src/components/UserProjectCard/OwnerProjectCard/index.jsx
+++ src/components/UserProjectCard/OwnerProjectCard/index.jsx
@@ -1,136 +1,190 @@
-import { FormControl, MenuItem, Select, Typography } from '@mui/material';
+import { FormControl, MenuItem, Select, Typography } from "@mui/material";
 import { alpha } from "@mui/material/styles";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./index.css";
 
 const projectStatus = {
-    0: { name: "Deleted", color: "var(--red)" },
-    1: { name: "Pending", color: "#FFC107" },
-    2: { name: "Processing", color: "#2196F3" },
-    3: { name: "Funded Successful", color: "var(--primary-green)" },
-    4: { name: "Successful", color: "var(--primary-green)" },
-    5: { name: "Failed", color: "var(--red)" },
-    6: { name: "Rejected", color: "var(--red)" },
-    7: { name: "Approved", color: "var(--primary-green)" },
-    8: { name: "Withdrawed", color: "#9C27B0" },
-    9: { name: "Refunded", color: "#FF5722" },
-    10: { name: "Reported", color: "#E91E63" },
+  0: { name: "Deleted", color: "var(--red)" },
+  1: { name: "Pending", color: "#FFC107" },
+  2: { name: "Processing", color: "#2196F3" },
+  3: { name: "Funded Successful", color: "var(--primary-green)" },
+  4: { name: "Successful", color: "var(--primary-green)" },
+  5: { name: "Failed", color: "var(--red)" },
+  6: { name: "Rejected", color: "var(--red)" },
+  7: { name: "Approved", color: "var(--primary-green)" },
+  8: { name: "Withdrawed", color: "#9C27B0" },
+  9: { name: "Refunded", color: "#FF5722" },
+  10: { name: "Reported", color: "#E91E63" },
 };
 
 function OwnerProjectCard({ project, projectType }) {
-    const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState("");
 
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
-    console.log(project);
-    return (
-        <div className="flex items-center rounded-md gap-[2rem]">
-            <div className="w-[10rem] h-[10rem] bg-[#EAEAEA] flex justify-center items-center rounded-lg">
-                <img src={projectType == "Funding" ? project?.fundingFiles?.find(p => p.filetype == 2 && p.isDeleted == false).url : project?.marketplaceFiles?.find(p => p.fileType == 2 && p.isDeleted == false).url} style={{ width: '10rem', height: '10rem', objectFit: 'cover', borderRadius: '5px' }} />
-            </div>
-            <div className="flex-grow !w-[12rem] h-fit">
-                <div className="flex items-center mb-[0.5rem] gap-[1rem]">
-                    <a href={projectType == "Funding" ? `/funding-detail/${project.id}` : `/marketplace-detail/${project.id}`}>
-                        <Typography
-                            sx={{
-                                color: '#2F3645',
-                                width: '12rem',
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                WebkitBoxOrient: 'vertical',
-                            }}
-                            className='user-project-card'
-                        >
-                            {project.name}
-                        </Typography>
-                    </a>
-                    <div className='flex items-center'>
-                        <span className="ml-[1rem] bg-[#1BAA64] text-[0.75rem] text-[#EAEAEA] px-[0.5rem] py-[0.25rem] rounded font-semibold"
-                            style={{ backgroundColor: projectStatus[project.status].color }}>
-                            {projectStatus[project.status].name}
-                        </span>
-                    </div>
-                </div>
-                <Typography sx={{ color: '#2F3645', fontWeight: '600', fontSize: '1rem', mb: '1.25rem' }} >
-                    by <span className='text-[#1BAA64]'>{project?.user.userName}</span>
-                </Typography>
-                <Typography
-                    sx={{
-                        color: '#2F3645',
-                        fontWeight: '300',
-                        fontSize: '1rem',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        display: '-webkit-box',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 2,
-                    }}
-                >
-                    {project.description}
-                </Typography>
-            </div>
-            <FormControl sx={{
-                minWidth: '8rem', height: '2.5rem',
-                '.MuiOutlinedInput-notchedOutline': { border: '0 !important' },
-                "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
-                    border: '0 !important',
-                },
-                "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                    border: '0 !important',
-                },
-            }}>
-                <Select
-                    value={selectedValue}
-                    onChange={handleChange}
-                    displayEmpty
-                    renderValue={(value) =>
-                        value ? (
-                            <Typography sx={{ color: 'var(--black)' }}>Action</Typography>
-                        ) : (
-                            <Typography sx={{ color: 'var(--black)' }}>Action</Typography>
-                        )
-                    }
-                    sx={{
-                        backgroundColor: '#EAEAEA',
-                        border: 'none !important',
-                        display: 'flex',
-                        alignItems: 'center',
-                        '& .MuiSelect-select': {
-                            padding: '0 1rem',
-                            display: 'flex',
-                            alignItems: 'center',
-                            height: '2.5rem',
-                        },
-                        '&:hover': {
-                            backgroundColor: alpha('#EAEAEA', 0.85),
-                        },
-                        height: '2.5rem',
-                    }}
-                    MenuProps={{
-                        PaperProps: {
-                            style: {
-                                maxHeight: 200,
-                                minWidth: '12rem',
-                                marginTop: '0.5rem',
-                            },
-                        },
-                    }}
-                >
-                    <MenuItem value="edit" onClick={() => window.open(`/account/projects/update/${project.id}/basic-info`, '_blank')}>
-                        <Typography>Edit campaign</Typography>
-                    </MenuItem>
-                    <MenuItem value="remove">
-                        <Typography>Remove campaign</Typography>
-                    </MenuItem>
-                    {project.status == 4 &&
-                        <MenuItem value="publish" onClick={() => window.open(`/request-marketplace-project/${project.id}/basic-info`, '_blank')}>
-                            <Typography>Publish to marketplace</Typography>
-                        </MenuItem>}
-                </Select>
-            </FormControl>
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+  console.log(project);
+  return (
+    <div className="flex items-center rounded-md gap-[2rem]">
+      <div className="w-[10rem] h-[10rem] bg-[#EAEAEA] flex justify-center items-center rounded-lg">
+        <img
+          src={
+            projectType == "Funding"
+              ? project?.fundingFiles?.find(
+                  (p) => p.filetype == 2 && p.isDeleted == false
+                ).url
+              : project?.marketplaceFiles?.find(
+                  (p) => p.fileType == 2 && p.isDeleted == false
+                ).url
+          }
+          style={{
+            width: "10rem",
+            height: "10rem",
+            objectFit: "cover",
+            borderRadius: "5px",
+          }}
+        />
+      </div>
+      <div className="flex-grow !w-[12rem] h-fit">
+        <div className="flex items-center mb-[0.5rem] gap-[1rem]">
+          <a
+            href={
+              projectType == "Funding"
+                ? `/funding-detail/${project.id}`
+                : `/marketplace-detail/${project.id}`
+            }
+          >
+            <Typography
+              sx={{
+                color: "#2F3645",
+                width: "12rem",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                WebkitBoxOrient: "vertical",
+              }}
+              className="user-project-card"
+            >
+              {project.name}
+            </Typography>
+          </a>
+          <div className="flex items-center">
+            <span
+              className="ml-[1rem] bg-[#1BAA64] text-[0.75rem] text-[#EAEAEA] px-[0.5rem] py-[0.25rem] rounded font-semibold"
+              style={{ backgroundColor: projectStatus[project.status].color }}
+            >
+              {projectStatus[project.status].name}
+            </span>
+          </div>
         </div>
-    );
+        <Typography
+          sx={{
+            color: "#2F3645",
+            fontWeight: "600",
+            fontSize: "1rem",
+            mb: "1.25rem",
+          }}
+        >
+          by <span className="text-[#1BAA64]">{project?.user.userName}</span>
+        </Typography>
+        <Typography
+          sx={{
+            color: "#2F3645",
+            fontWeight: "300",
+            fontSize: "1rem",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: 2,
+          }}
+        >
+          {project.description}
+        </Typography>
+      </div>
+      <FormControl
+        sx={{
+          minWidth: "8rem",
+          height: "2.5rem",
+          ".MuiOutlinedInput-notchedOutline": { border: "0 !important" },
+          "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+            border: "0 !important",
+          },
+          "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+            {
+              border: "0 !important",
+            },
+        }}
+      >
+        <Select
+          value={selectedValue}
+          onChange={handleChange}
+          displayEmpty
+          renderValue={(value) =>
+            value ? (
+              <Typography sx={{ color: "var(--black)" }}>Action</Typography>
+            ) : (
+              <Typography sx={{ color: "var(--black)" }}>Action</Typography>
+            )
+          }
+          sx={{
+            backgroundColor: "#EAEAEA",
+            border: "none !important",
+            display: "flex",
+            alignItems: "center",
+            "& .MuiSelect-select": {
+              padding: "0 1rem",
+              display: "flex",
+              alignItems: "center",
+              height: "2.5rem",
+            },
+            "&:hover": {
+              backgroundColor: alpha("#EAEAEA", 0.85),
+            },
+            height: "2.5rem",
+          }}
+          MenuProps={{
+            PaperProps: {
+              style: {
+                maxHeight: 200,
+                minWidth: "12rem",
+                marginTop: "0.5rem",
+              },
+            },
+          }}
+        >
+          <MenuItem
+            value="edit"
+            onClick={() =>
+              window.open(
+                `/account/${
+                  projectType == "Funding" ? "projects" : "marketplace-projects"
+                }/update/${project.id}/basic-info`,
+                "_blank"
+              )
+            }
+          >
+            <Typography>Edit campaign</Typography>
+          </MenuItem>
+          <MenuItem value="remove">
+            <Typography>Remove campaign</Typography>
+          </MenuItem>
+          {project.status == 4 && (
+            <MenuItem
+              value="publish"
+              onClick={() =>
+                window.open(
+                  `/request-marketplace-project/${project.id}/basic-info`,
+                  "_blank"
+                )
+              }
+            >
+              <Typography>Publish to marketplace</Typography>
+            </MenuItem>
+          )}
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
 
 export default OwnerProjectCard;
--- src/pages/UpdateMarketplaceProjectForm/MarketplaceProjectCoupon/index.jsx
+++ src/pages/UpdateMarketplaceProjectForm/MarketplaceProjectCoupon/index.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable no-unused-vars */
+import { Box, Button } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import CouponsTable from "../../../components/CouponsTable";
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import couponApiInstace from "../../../utils/ApiInstance/couponApiInstance";
+import Swal from "sweetalert2";
+import Cookies from "js-cookie";
+import { useLoading } from "../../../contexts/LoadingContext";
 
 const MarketplaceProjectCoupon = () => {
   const [data, setData] = useState([]);
+  const [couponFile, setCouponFile] = useState(null);
   const { id } = useParams();
+  const { isLoading, setIsLoading } = useLoading();
+  const token = Cookies.get("_auth");
+
   const fetchData = async () => {
     try {
       const response = await couponApiInstace.get(`/all/${id}`);
@@ -16,20 +25,136 @@ const MarketplaceProjectCoupon = () => {
       console.log(error);
     }
   };
+
   const mappingData = data.map((item) => ({
     Id: item.id,
     "Coupon Key": item.couponKey,
     "Coupon Name": item.couponName,
     "Discount Rate": item.discountRate,
-    Status: item.status === 1 ? "Active" : "1",
+    Status: item.status === 1 ? "Enabled" : "Disabled",
   }));
+
   useEffect(() => {
     fetchData();
   }, []);
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setCouponFile(file);
+      console.log(file);
+      e.target.value = null;
+    }
+  };
+
+  const handleSave = async () => {
+    if (couponFile) {
+      setIsLoading(true);
+
+      const formData = new FormData();
+
+      formData.append("formFile", couponFile);
+
+      try {
+        const response = await couponApiInstace.post(
+          `?projectId=${id}`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          Swal.fire({
+            title: "Success",
+            text: response.data._message[0],
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+
+          fetchData();
+          setCouponFile(null);
+        }
+      } catch (error) {
+        console.log(error);
+        if (error.response) {
+          Swal.fire({
+            title: "Error",
+            text: error.response.data.message,
+            icon: "error",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
   return (
     <>
       <div className="m-2 p-4">
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "1rem",
+            }}
+          >
+            <input
+              type="file"
+              onChange={(e) => handleFileChange(e)}
+              style={{ display: "none" }}
+              id="file-input"
+            />
+            <Button
+              variant="contained"
+              onClick={() => document.getElementById("file-input").click()}
+              sx={{
+                backgroundColor: "#1BAA64",
+                textTransform: "none",
+                fontWeight: "600",
+              }}
+              startIcon={<AddCircleIcon />}
+            >
+              Import File
+            </Button>
+            <a href={couponFile ? URL.createObjectURL(couponFile) : ""}>
+              {couponFile ? couponFile.name : ""}
+            </a>
+          </Box>
+          <Box>
+            <Button
+              variant="contained"
+              onClick={() => {
+                handleSave();
+              }}
+              sx={{
+                backgroundColor: "#1BAA64",
+                textTransform: "none",
+                fontWeight: "600",
+                display: `${couponFile ? "block" : "none"}`,
+              }}
+            >
+              Save
+            </Button>
+          </Box>
+        </Box>
         <h1 className="text-3xl font-bold mb-6 text-center text-[#1BAA64]">
           Coupons
         </h1>

f9cbb7528f70a68817699be8b40fa62280523cfb
baocd2003 <BaoCao494@gmail.com>
2024-11-30 20:06:04 +0700

    update minor milestone

--- src/pages/UpdateFundingProjectForm/ProjectMilestone/Milestone1/index.jsx
+++ src/pages/UpdateFundingProjectForm/ProjectMilestone/Milestone1/index.jsx
@@ -1,30 +1,21 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
 import axios from "axios";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation } from 'react-router-dom';
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import {
-  Backdrop,
-  CircularProgress,
-  Box,
-  Button,
-  Divider,
-  FormControl,
-  MenuItem,
-  Select,
-  TextField,
-  Typography,
-  Tab,
-} from "@mui/material";
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
+  Backdrop, CircularProgress, Box, Button, Divider, FormControl,
+  MenuItem, Select, TextField, Typography, Tab
+} from '@mui/material';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import { checkAvailableMilestone } from "../../../../utils/Hooks/checkAvailableMilestone";
 import projectMilestoneApiInstace from "../../../../utils/ApiInstance/projectMilestoneApiInstance";
 import UpdateMilestone from "../UpdateMilestone";
 import MilestoneQuill from "../../../../components/UpdateProject/MilestoneQuill";
 import FileUploadDropdown from "../../../../components/UpdateProject/UploadFiles/FileUploadDropdown";
-import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 import milestoneApiInstace from "../../../../utils/ApiInstance/milestoneApiInstance";
 import BackdropRequestMilestone from "../../../../components/UpdateProject/BackdropRequestMilestone";
 import Swal from "sweetalert2";
@@ -44,9 +35,9 @@ const MilestoneForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [dropdownAnchorEl, setDropdownAnchorEl] = useState(null);
-  const [anchorEls, setAnchorEls] = useState({});
+  const [anchorEls, setAnchorEls] = useState({})
   const [isBackdropHidden, setIsBackdropHidden] = useState(false);
-  const [buttonActive, setButtonActive] = useState(false);
+  const [buttonActive, setButtonActive] = useState(false)
   const [issueLog, setIssueLog] = useState("");
   const [daysLeft, setDaysLeft] = useState(0);
   //check available project milestone
@@ -64,10 +55,10 @@ const MilestoneForm = () => {
       if (data.status === 'create' || data.status === 'edit' || data.status === 'warning') {
         setIsLoading(false)
       } else {
-        setIsBackdropHidden(true);
+        setIsBackdropHidden(true)
       }
     } catch (error) {
-      console.error("Error fetching milestone data:", error);
+      console.error('Error fetching milestone data:', error);
     } finally {
       setIsLoading(false); // Stop loading once data fetch is complete
     }
@@ -126,14 +117,8 @@ const MilestoneForm = () => {
     console.log(formDataArray);
     const data = new FormData();
     formDataArray.forEach((formData, i) => {
-      data.append(
-        `request[${i}].RequirementStatus`,
-        formData.requirementStatus
-      );
-      data.append(
-        `request[${i}].UpdateDate`,
-        formData.updateDate.toISOString()
-      );
+      data.append(`request[${i}].RequirementStatus`, formData.requirementStatus);
+      data.append(`request[${i}].UpdateDate`, formData.updateDate.toISOString());
       data.append(`request[${i}].Content`, formData.content);
       data.append(`request[${i}].MilestoneId`, formData.milestoneId);
       data.append(`request[${i}].FundingProjectId`, formData.fundingProjectId);
@@ -141,14 +126,8 @@ const MilestoneForm = () => {
 
       formData.requirementFiles.forEach((file, fileIndex) => {
         data.append(`request[${i}].RequirementFiles[${fileIndex}].URL`, file);
-        data.append(
-          `request[${i}].RequirementFiles[${fileIndex}].Name`,
-          file.name
-        );
-        data.append(
-          `request[${i}].RequirementFiles[${fileIndex}].Filetype`,
-          categorizeFileType(file.type)
-        );
+        data.append(`request[${i}].RequirementFiles[${fileIndex}].Name`, file.name);
+        data.append(`request[${i}].RequirementFiles[${fileIndex}].Filetype`, categorizeFileType(file.type));
       });
     });
     data.append("issueLog", issueLog);
@@ -161,13 +140,13 @@ const MilestoneForm = () => {
       Swal.fire({
         title: "You have been successfully updated evidence for this milestone",
         text: "Milestone created successfully",
-        icon: "success",
+        icon: "success"
       });
     } catch (error) {
       Swal.fire({
         title: "Error",
         text: "Failed to submit requirements",
-        icon: "erro",
+        icon: "erro"
       });
       alert("Failed to submit requirements.");
     } finally {
@@ -180,6 +159,7 @@ const MilestoneForm = () => {
   //dropdown files
   const [anchorEl, setAnchorEl] = useState(null); // Tracks dropdown state
   const [currentIndex, setCurrentIndex] = useState(null); // Tracks which requirement files are open
+
 
   const handleFilesSelected = (selectedFiles, index) => {
     const updatedFormData = [...formDataArray];
@@ -210,7 +190,7 @@ const MilestoneForm = () => {
     setFormDataArray(updatedFormData);
   };
 
-  const [value, setValue] = React.useState("1");
+  const [value, setValue] = React.useState('1');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -219,9 +199,9 @@ const MilestoneForm = () => {
   if (!milestone || !milestoneData) return <p>Loading milestone...</p>;
 
   return (
-    <div>
-      {milestoneData && milestone && !isLoading && (
-        <BackdropRequestMilestone
+    <div >
+      {milestoneData && milestone && !isLoading
+        && <BackdropRequestMilestone
           isHidden={isBackdropHidden}
           projectId={projectId}
           milestone={milestone}
@@ -229,27 +209,21 @@ const MilestoneForm = () => {
           onCloseBackdrop={handleBackdropClose}
           render={() => getMilestoneData(milestoneId)} />}
       {/* Backdrop with loading spinner */}
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={false}
-      >
+      <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={false}>
         <CircularProgress color="inherit" />
       </Backdrop>
-      <div className="basic-info-section">
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingRight: "5rem",
-          }}
-        >
+      <div className='basic-info-section'>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingRight: '5rem' }}>
           <Box>
-            <Typography className="basic-info-title">
-              {milestone.milestoneName}
-              <span className="text-[#1BAA64]">*</span>
+            <Typography
+              className='basic-info-title'
+            >
+              {milestone.milestoneName}<span className='text-[#1BAA64]'>*</span>
             </Typography>
-            <Typography className="basic-info-subtitle" sx={{ width: "100%" }}>
+            <Typography
+              className='basic-info-subtitle'
+              sx={{ width: '100%' }}
+            >
               {milestone.description}
             </Typography>
           </Box>
@@ -259,14 +233,12 @@ const MilestoneForm = () => {
 
         </Box>
 
-        {!isLoading && milestoneData && milestoneData.status == "create" ? (
+        {!isLoading && milestoneData && milestoneData.status == 'create' ? (
+
           <form onSubmit={handleSubmit}>
             <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <TabList
-                  onChange={handleChange}
-                  aria-label="lab API tabs example"
-                >
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList onChange={handleChange} aria-label="lab API tabs example">
                   <Tab label="Requirements" value="1" />
                   <Tab label="Issue Logs" value="2" />
                 </TabList>
@@ -274,38 +246,17 @@ const MilestoneForm = () => {
               <TabPanel value="1">
                 {milestone.requirements.map((req, index) => (
                   <div key={req.id} style={{ marginBottom: "20px" }}>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        width: "70%",
-                        marginBottom: "20px",
-                      }}
-                    >
-                      <h3 style={{ fontWeight: "600" }}>{req.title}</h3>
-                      <Button
-                        variant="contained"
-                        component="label"
-                        onClick={(e) => openDropdown(e, index)}
-                        sx={{
-                          backgroundColor: "#1BAA64",
-                          textTransform: "none",
-                          fontWeight: "600",
-                        }}
-                        startIcon={<ChangeCircleIcon />}
-                      >
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '70%', marginBottom: '20px' }}>
+                      <h3 style={{ fontWeight: '600' }}>{req.title}</h3>
+                      <Button variant="contained" component="label" onClick={(e) => openDropdown(e, index)}
+                        sx={{ backgroundColor: '#1BAA64', textTransform: 'none', fontWeight: '600' }} startIcon={<ChangeCircleIcon />}>
                         Upload Files
                         <input
                           type="file"
                           multiple
                           hidden
-                          onChange={(e) =>
-                            handleFilesSelected(
-                              Array.from(e.target.files),
-                              index
-                            )
-                          }
+                          onChange={(e) => handleFilesSelected(Array.from(e.target.files), index)}
+
                         />
                       </Button>
                     </Box>
@@ -316,27 +267,24 @@ const MilestoneForm = () => {
                         <div className="w-[70%]">
                           <MilestoneQuill
                             value={formDataArray[index]?.content || " "}
-                            onChange={(value) =>
-                              handleQuillChange(value, index)
-                            }
+                            onChange={(value) => handleQuillChange(value, index)}
                           />
                         </div>
-                        {formDataArray[index] &&
-                          formDataArray[index].requirementFiles && (
-                            <FileUploadDropdown
-                              uploadedFiles={
-                                formDataArray[index].requirementFiles
-                              }
-                              anchorEl={anchorEls[index]}
-                              onClose={() => closeDropdown(index)}
-                              onRemoveFile={(fileIndex) =>
-                                handleRemoveFile(fileIndex, index)
-                              }
-                              requirementFiles={[]}
-                            />
-                          )}
+                        {formDataArray[index] && formDataArray[index].requirementFiles && (
+                          <FileUploadDropdown
+                            uploadedFiles={formDataArray[index].requirementFiles}
+                            anchorEl={anchorEls[index]}
+                            onClose={() => closeDropdown(index)}
+                            onRemoveFile={(fileIndex) =>
+                              handleRemoveFile(fileIndex, index)
+                            }
+                            requirementFiles={[]}
+                          />
+                        )}
+
                       </>
                     </div>
+
                   </div>
                 ))}
               </TabPanel>
@@ -354,17 +302,12 @@ const MilestoneForm = () => {
               , textTransform: 'none', fontWeight: '600', width: '100px'
             }} type="submit">
               Save
-            </Button>
+            </Button >
           </form>
-        ) : (
-          <UpdateMilestone
-            render={() => getMilestoneData(milestoneId)}
-            milestones={milestoneData?.data[0]?.projectMilestoneRequirements}
-            issueLog={milestoneData?.data[0]?.issueLog}
-            pmId={milestoneData?.data[0]?.id}
-            status={milestoneData?.status}
-          />
-        )}
+        ) : <UpdateMilestone render={() => getMilestoneData(milestoneId)}
+          milestones={milestoneData?.data[0]?.projectMilestoneRequirements}
+          issueLog={milestoneData?.data[0]?.issueLog} pmId={milestoneData?.data[0]?.id}
+          status={milestoneData?.status} />}
       </div>
     </div>
   );

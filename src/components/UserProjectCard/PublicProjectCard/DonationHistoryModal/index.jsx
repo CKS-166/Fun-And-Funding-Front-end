6526b5618e24c40822753d3a6e425b6832b152a3
diemyolo <quydiem2015@gmail.com>
2024-12-17 13:23:27 +0700

    Add-Donation-History

---
+++ src/components/UserProjectCard/PublicProjectCard/DonationHistoryModal/index.jsx
@@ -0,0 +1,200 @@
+import { Box, Button, CircularProgress, Dialog, Fade, Modal, Paper, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import NoImage from '../../../../assets/images/no-image.png';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '60%',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    px: '2rem',
+    py: '2rem',
+    minHeight: '90%',
+    borderRadius: 1
+};
+
+const packageType = {
+    0: { name: "Free", backgroundColor: "var(--grey)", color: "var(--white)" },
+    1: { name: "Package", backgroundColor: "var(--primary-green)", color: "var(--white)" },
+}
+
+function DonationHistoryModal({ donationList, openModal, handleClose, project }) {
+    const [open, setOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState([]);
+
+    const handleClickOpen = (item) => {
+        setSelectedItem(item);
+        setOpen(true);
+    }
+
+    const handleCloseDiaglog = () => {
+        setSelectedItem({});
+        setOpen(false);
+    };
+    return (
+        <>
+            <Modal open={openModal} onClose={handleClose}>
+                <Fade in={openModal}>
+                    <Box sx={style}>
+                        {donationList != null && donationList.length > 0 &&
+                            <>
+                                <div className="mb-[1.5rem] mx-[0.5rem]">
+                                    <Typography sx={{ fontSize: '1.5rem', fontWeight: '700' }}>
+                                        Donation History
+                                    </Typography>
+                                    <Typography sx={{ mt: '0.5rem', fontSize: '1rem', fontWeight: 400 }}>
+                                        Showing your package donation history in{" "}
+                                        <span className="text-[var(--primary-green)] font-semibold">
+                                            {project?.name}
+                                        </span>
+                                    </Typography>
+                                </div>
+                                <div className='mx-[0.5rem]'>
+                                    <Paper
+                                        sx={{
+                                            width: '100%',
+                                            p: '1rem',
+                                            height: '2',
+                                            borderRadius: '0.625rem',
+                                            boxShadow: '0px 3px 3px -2px rgba(0, 0, 0, 0.2), 0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12)',
+                                            display: 'flex',
+                                            flexDirection: 'row',
+                                            justifyContent: 'space-between',
+                                            gap: '0.5rem',
+                                            mb: '0.5rem'
+                                        }}
+                                        elevation={3}
+                                    >
+                                        <Typography sx={{ width: '15%', fontWeight: 600 }}>Image</Typography>
+                                        <Typography sx={{ width: '25%', fontWeight: 600 }}>Name</Typography>
+                                        <Typography sx={{ width: '15%', fontWeight: 600 }}>Date</Typography>
+                                        <Typography sx={{ width: '10%', mr: '0.5rem', fontWeight: 600 }}>Type</Typography>
+                                        <Typography sx={{ width: '25%', fontWeight: 600 }}>Amount</Typography>
+                                        <Typography sx={{ width: '10%', fontWeight: 600 }}>Items</Typography>
+                                    </Paper>
+                                </div>
+                                <Box
+                                    sx={{
+                                        height: '27rem',
+                                        overflowY: 'auto',
+                                        overflowX: 'visible',
+                                        px: '0.5rem',
+                                        scrollbarWidth: 'none',
+                                        '&::-webkit-scrollbar': {
+                                            display: 'none'
+                                        },
+                                        pb: '1rem'
+                                    }}
+                                >
+                                    {donationList.map((item, index) => <Paper
+                                        index={index}
+                                        sx={{
+                                            width: '100%',
+                                            mt: '2rem',
+                                            p: '1rem',
+                                            height: 'fit-content',
+                                            borderRadius: '0.625rem',
+                                            boxShadow: '0px 3px 3px -2px rgba(0, 0, 0, 0.2), 0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12)',
+                                            display: 'flex',
+                                            flexDirection: 'row',
+                                            justifyContent: 'space-between',
+                                            alignItems: 'center',
+                                            gap: '0.5rem'
+                                        }}
+                                        elevation={3}
+                                    >
+                                        <div className='w-[15%]'>
+                                            <div className="overflow-hidden flex justify-start items-center w-fit">
+                                                <img class=" object-cover rounded-[0.625rem] h-[6rem]" alt="Package image" src={item.packageUrl == '' ? NoImage : item.packageUrl} />
+                                            </div>
+                                        </div>
+                                        <Typography
+                                            sx={{
+                                                display: '-webkit-box',
+                                                WebkitBoxOrient: 'vertical',
+                                                overflow: 'hidden',
+                                                textOverflow: 'ellipsis',
+                                                WebkitLineClamp: 3,
+                                                width: '25%',
+                                                fontWeight: 600,
+                                                maxWidth: '25%',
+                                            }}
+                                        >
+                                            {item.packageName}
+                                        </Typography>
+                                        <Typography sx={{ width: '15%', fontWeight: 400 }}>{new Date(item.createDate).toLocaleString()}</Typography>
+                                        <Typography sx={{ width: '10%', mr: '0.5rem', fontWeight: 600, backgroundColor: packageType[item.types].backgroundColor, color: packageType[item.types].color, textAlign: 'center', fontSize: '0.75rem', height: 'fit-content', borderRadius: '0.625rem', py: '0.25rem' }}>{packageType[item.types].name}</Typography>
+                                        <Typography sx={{ width: '25%', fontWeight: 600 }}>{new Intl.NumberFormat('de-DE').format(item.donateAmount)} VND</Typography>
+                                        <Typography sx={{ width: '10%', fontWeight: 600 }}>
+                                            <Button
+                                                variant="contained"
+                                                sx={{ backgroundColor: "#1BAA64", textTransform: "none", visibility: item.types == 0 ? 'hidden' : 'visible' }}
+                                                onClick={() => handleClickOpen(item.rewardItems)}
+                                            >
+                                                Show
+                                            </Button>
+                                        </Typography>
+                                    </Paper>)}
+                                </Box>
+                            </>
+                        }
+                    </Box>
+                </Fade>
+            </Modal>
+            <Dialog
+                open={open}
+                onClose={handleCloseDiaglog}
+                sx={{
+                    '& .MuiDialog-paper': {
+                        width: '30rem',
+                        maxWidth: '30rem',
+                        p: '2rem',
+                    },
+                }}
+            >
+                <Typography sx={{ fontSize: '1.5rem', fontWeight: '700', width: '100%', textAlign: 'left' }}>
+                    Reward Item List
+                </Typography>
+                <Typography sx={{ mt: '0.5rem', fontSize: '1rem', fontWeight: 400, mb: '2rem' }}>
+                    Showing all package reward items
+                </Typography>
+                <Box sx={{
+                    overflowY: 'auto',
+                    maxHeight: '25rem',
+                    scrollbarWidth: 'none',
+                    '&::-webkit-scrollbar': {
+                        display: 'none',
+                    },
+                }}>
+                    {selectedItem != null && selectedItem.length > 0 ?
+                        selectedItem.map((item, index) =>
+                            <div className='flex justify-between items-center mb-[1.5rem]' key={index}>
+                                <img src={item.imageUrl ?? NoImage} style={{ width: '4rem', height: '4rem', objectFit: 'cover', borderRadius: '0.625rem' }} />
+                                <div className='flex flex-col justify-start'>
+                                    <Typography sx={{ fontSize: '1.25rem', fontWeight: '700', mb: '0.25rem', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', width: '12rem' }}>
+                                        {item.rewardName}
+                                    </Typography>
+                                    <Typography sx={{ fontSize: '0.75rem', fontWeight: '400', mb: '0.5rem', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', width: '12rem' }}>
+                                        {item.description}
+                                    </Typography>
+                                </div>
+                                <div className='my-auto w-[6rem]'>
+                                    <Typography sx={{ fontSize: '1rem', fontWeight: '500', mb: '0.25rem', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', width: '6rem', textAlign: 'right' }}>
+                                        Count: {item.quantity}
+                                    </Typography>
+                                </div>
+                            </div>
+                        ) : <div className='w-full flex justify-center'>
+                            <CircularProgress sx={{ color: 'var(--grey)' }} />
+                        </div>
+                    }
+                </Box>
+            </Dialog>
+        </>
+    )
+}
+
+export default DonationHistoryModal
--- src/components/UserProjectCard/PublicProjectCard/index.jsx
+++ src/components/UserProjectCard/PublicProjectCard/index.jsx
@@ -1,5 +1,8 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import { Button, Typography } from '@mui/material';
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from 'react';
+import packageBackerApiInstance from "../../../utils/ApiInstance/packageBackerApiInstance";
+import DonationHistoryModal from './DonationHistoryModal';
 
 const projectStatus = {
     0: { name: "Deleted", color: "var(--red)" },
@@ -16,54 +19,92 @@ const projectStatus = {
 };
 
 function PublicProjectCard({ project, projectType }) {
+    const token = Cookies.get("_auth");
+    const [openModal, setOpenModal] = useState(false);
+    const [donationHistory, setDonationHistory] = useState([])
+
+    const handleOpen = () => setOpenModal(true);
+    const handleClose = () => setOpenModal(false);
+
+    useEffect(() => {
+        if (projectType == "Funding") {
+            fetchDonationHistory();
+        }
+    }, [project])
+
+    const fetchDonationHistory = async () => {
+        try {
+            const res = await packageBackerApiInstance.get(`donated-project?projectId=${project.id}`, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            if (res.data._statusCode == 200) {
+                console.log(res.data._data);
+                setDonationHistory(res.data._data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
-        <div className="flex items-center rounded-lg gap-[2rem]">
-            <div className="w-[10rem] h-[10rem] bg-[#EAEAEA] flex justify-center items-center rounded-lg">
-                <img src={projectType == "Funding" ? project?.fundingFiles?.find(p => p.filetype == 2 && p.isDeleted == false).url : project?.marketplaceFiles?.find(p => p.fileType == 2 && p.isDeleted == false).url} style={{ width: '10rem', height: '10rem', objectFit: 'cover', borderRadius: '0.625rem' }} />
-            </div>
-            <div className="flex-grow !w-[12rem] h-fit">
-                <div className="flex items-center mb-[0.5rem] gap-[1rem]">
-                    <a href={projectType == "Funding" ? `/funding-detail/${project.id}` : `/marketplace-detail/${project.id}`}>
-                        <Typography
-                            sx={{
-                                color: '#2F3645',
-                                width: "25rem",
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                WebkitBoxOrient: 'vertical',
-                            }}
-                            className='user-project-card'
-                        >
-                            {project.name}
-                        </Typography>
-                    </a>
-                    <div className='flex items-center'>
-                        <span className="ml-[1rem] bg-[#1BAA64] text-[0.75rem] text-[#EAEAEA] px-[0.5rem] py-[0.25rem] rounded font-semibold"
-                            style={{ backgroundColor: projectStatus[project.status].color }}>
-                            {projectStatus[project.status].name}
-                        </span>
-                    </div>
+        <>
+            <div className="flex items-center rounded-lg gap-[2rem]">
+                <div className="w-[10rem] h-[10rem] bg-[#EAEAEA] flex justify-center items-center rounded-lg">
+                    <img src={projectType == "Funding" ? project?.fundingFiles?.find(p => p.filetype == 2 && p.isDeleted == false).url : project?.marketplaceFiles?.find(p => p.fileType == 2 && p.isDeleted == false).url} style={{ width: '10rem', height: '10rem', objectFit: 'cover', borderRadius: '0.625rem' }} />
                 </div>
-                <Typography sx={{ color: '#2F3645', fontWeight: '600', fontSize: '1rem', mb: '1.25rem' }} >
-                    by <span className='text-[#1BAA64]'>{project?.user.userName}</span>
-                </Typography>
-                <Typography
-                    sx={{
-                        color: '#2F3645',
-                        fontWeight: '300',
-                        fontSize: '1rem',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        display: '-webkit-box',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 2,
-                    }}
-                >
-                    {project.description}
-                </Typography>
+                <div className="flex-grow !w-[12rem] h-fit">
+                    <div className="flex items-center mb-[0.5rem] gap-[1rem]">
+                        <a href={projectType == "Funding" ? `/funding-detail/${project.id}` : `/marketplace-detail/${project.id}`}>
+                            <Typography
+                                sx={{
+                                    color: '#2F3645',
+                                    maxWidth: "25rem",
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    WebkitBoxOrient: 'vertical',
+                                }}
+                                className='user-project-card'
+                            >
+                                {project.name}
+                            </Typography>
+                        </a>
+                        <div className='flex items-center'>
+                            <span className="ml-[1rem] bg-[#1BAA64] text-[0.75rem] text-[#EAEAEA] px-[0.5rem] py-[0.25rem] rounded font-semibold"
+                                style={{ backgroundColor: projectStatus[project.status].color }}>
+                                {projectStatus[project.status].name}
+                            </span>
+                        </div>
+                    </div>
+                    <Typography sx={{ color: '#2F3645', fontWeight: '600', fontSize: '1rem', mb: '1.25rem' }} >
+                        by <span className='text-[#1BAA64]'>{project?.user.userName}</span>
+                    </Typography>
+                    <Typography
+                        sx={{
+                            color: '#2F3645',
+                            fontWeight: '300',
+                            fontSize: '1rem',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            display: '-webkit-box',
+                            WebkitBoxOrient: 'vertical',
+                            WebkitLineClamp: 2,
+                        }}
+                    >
+                        {project.description}
+                    </Typography>
+                </div>
+                <div className={`min-w-[8rem] ${projectType == "Funding" ? 'visible' : 'hidden'}`}>
+                    <Button
+                        variant="contained"
+                        sx={{ backgroundColor: "#1BAA64", textTransform: "none" }}
+                        onClick={() => handleOpen()}
+                    >
+                        Show Donation History
+                    </Button>
+                </div>
             </div>
-        </div>
+            <DonationHistoryModal donationList={donationHistory} openModal={openModal} handleClose={handleClose} project={project} />
+        </>
     );
 }
 

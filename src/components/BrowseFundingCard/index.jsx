d96f35bd26d2183a327c7f82f1f0d159e53b343c
SuongCao <caokhasuong2003@gmail.com>
2025-01-11 11:01:08 +0700

    fix investor=0

--- src/components/BrowseFundingCard/index.jsx
+++ src/components/BrowseFundingCard/index.jsx
@@ -167,11 +167,12 @@ function BrowseFundingCard({ fundingProject }) {
                         <BorderLinearProgress variant="determinate" sx={{ width: "100%", my: '0.313rem' }} value={convertPercentage(fundingProject.balance, fundingProject.target) <= 100 ? convertPercentage(fundingProject.balance, fundingProject.target) : 100} />
                         <div className='flex flex-row justify-between'>
                             <Typography sx={{ color: '#2F3645', fontSize: '0.75rem', fontWeight: '600' }}>
-                                {packBackers?.length} Investors
-                            </Typography>
-                            <Typography sx={{ color: '#2F3645', fontSize: '0.75rem', fontWeight: '600' }}>
                                 {convertPercentage(fundingProject.balance, fundingProject.target)}% of target
                             </Typography>
+                            <Typography sx={{ color: '#2F3645', fontSize: '0.75rem', fontWeight: '600' }}>
+                                {/* {packBackers?.length} Investors */}
+                            </Typography>
+
                         </div>
                     </div>
                 </Card>
